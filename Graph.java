680f6cd9ff1ad3629339db46eef054baac2690ad
yashp3100 <yashp13100@gmail.com>
2024-12-13 11:28:48 -0600

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,1 +1,138 @@
+package HW8;
+
+
+/******************************************************************
+ *
+ *   YOUR NAME / SECTION NUMBER
+ *   Yash Patel - 001
+ *   Note, additional comments provided throughout this source code
+ *   is for educational purposes
+ *
+ ********************************************************************/
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+/**
+ *  Graph traversal exercise
+ *
+ *  The Graph class is a representing an oversimplified Directed Graph of vertices
+ *  (nodes) and edges. The graph is stored in an adjacency list
+ */
+
+public class Graph {
+    int numVertices;                  // vertices in graph
+    LinkedList<Integer>[] adjListArr; // Adjacency list
+    List<Integer> vertexValues;       // vertex values
+
+    // Constructor
+    public Graph(int numV) {
+        numVertices = numV;
+        adjListArr = new LinkedList[numVertices];
+        vertexValues = new ArrayList<>(numVertices);
+
+        for (int i = 0; i < numVertices; i++) {
+            adjListArr[i] = new LinkedList<>();
+            vertexValues.add(0);
+        }
+    }
+
+    /*
+     * method setValue
+     *
+     * Sets a vertex's (node's) value.
+     */
+
+    public void setValue(int vertexIndex, int value) {
+        if (vertexIndex >= 0 && vertexIndex < numVertices) {
+            vertexValues.set(vertexIndex, value);
+        } else {
+            throw new IllegalArgumentException(
+                    "Invalid vertex index: " + vertexIndex);
+        }
+    }
+
+
+    public void addEdge(int src, int dest) {
+        adjListArr[src].add(dest);
+    }
+
+    /*
+     * method printGraph
+     *
+     * Prints the graph as an adjacency matrix
+     */
+
+    public void printGraph() {
+        System.out.println(
+                "\nAdjacency Matrix Representation:\n");
+        int[][] matrix = new int[numVertices][numVertices];
+
+        for (int i = 0; i < numVertices; i++) {
+            for (Integer dest : adjListArr[i]) {
+                matrix[i][dest] = 1;
+            }
+        }
+
+        System.out.print("  ");
+        for (int i = 0; i < numVertices; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < numVertices; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < numVertices; j++) {
+                if (matrix[i][j] == 1) {
+                    System.out.print("| ");
+                } else {
+                    System.out.print(". ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+    /**
+     * method findRoot
+     * <p>
+     * This method returns the value of the root vertex, where root is defined in
+     * this case as a node that has no incoming edges. If no root vertex is found
+     * and/or more than one root vertex, then return -1.
+     */
+
+    public int findRoot() {
+        int[] inDegree = new int[numVertices];
+
+        // Calculate in-degrees for each vertex
+        for (int i = 0; i < numVertices; i++) {
+            for (int neighbor : adjListArr[i]) {
+                inDegree[neighbor]++;
+            }
+        }
+
+        int rootCount = 0;
+        int rootIndex = -1;
+
+        // Identify the vertex with in-degree 0
+        for (int i = 0; i < numVertices; i++) {
+            if (inDegree[i] == 0) {
+                rootCount++;
+                rootIndex = i;
+            }
+        }
+
+        // If there is exactly one root, return its value
+        if (rootCount == 1) {
+            return vertexValues.get(rootIndex);
+        }
+
+        // Otherwise, return -1 (no root or multiple roots)
+        return -1;
+    }
+}
+
 
